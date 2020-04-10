bc9179b34e4117d45bc63056b47b9fa28d41c717
Animartonn <63472776+Animartonn@users.noreply.github.com>
2020-04-11 00:26:43 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const cartButton = document.querySelector("#cart_button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", function (event) {
+    modal.classList.add("is_open");
+    
+});
+
+close.addEventListener("click", function (event) {
+    modal.classList.remove("is_open");
+
+});
+
+function toggleModal() {
+    modal.classList.toggle('is_open')
+}
+
+new WOW().init();
+
